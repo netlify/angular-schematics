4ec7c63bb0adadac98651c5d9fdfde304d4875d3
Tara Z. Manicsic <tzmanics@gmail.com>
2020-02-27 15:36:59 -0500

    adds options object

--- src/netlify-schematics/index.ts
+++ src/netlify-schematics/index.ts
@@ -1,13 +1,13 @@
 import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
 
-export function netlifySchematics(_options: any): Rule {
+export function netlifySchematics(options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     const configData = {
-      apiId: "",
-      accessToken: "",
-      projectName: "",
-      publish: "",
-      command: ""
+      apiId: options.apiId,
+      accessToken: options.accessToken,
+      projectName: options.projectName,
+      publish: options.publish,
+      command: options.command
     };
 
     tree.create("/netlify.json", JSON.stringify(configData, null, 2));
--- src/netlify-schematics/index_spec.ts
+++ src/netlify-schematics/index_spec.ts
@@ -7,8 +7,20 @@ const collectionPath = path.join(__dirname, "../collection.json");
 describe("ng-add", () => {
   it("creates config file", () => {
     const runner = new SchematicTestRunner("schematics", collectionPath);
-    const tree = runner.runSchematic("netlify-schematics", {}, Tree.empty());
+    const options = {
+      apiId: "apiId",
+      accessToken: "accessToken",
+      projectName: "projectName",
+      publish: "publish",
+      command: "command"
+    };
+    const tree = runner.runSchematic("ng-add", options, Tree.empty());
+    const content = tree.readContent("/netlify.json");
 
-    expect(tree.files).toEqual(["/netlify.json"]);
+    expect(content).toContain('apiId": "apiId"');
+    expect(content).toContain('accessToken": "accessToken"');
+    expect(content).toContain('projectName": "projectName"');
+    expect(content).toContain('publish": "publish"');
+    expect(content).toContain('command": "command"');
   });
 });
