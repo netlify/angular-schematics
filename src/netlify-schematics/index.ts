1ac3cddb3aa19e391c8e11970b980861b07a63a0
Tara Z. Manicsic <tara@netlify.com>
2020-12-23 16:42:27 -0500

    amends or ovewrites netlify.toml (#7)
    * checks if netlify.toml exists and does not overwrite
    
    * adds conditional amend or overwrite of toml

--- src/netlify-schematics/index.ts
+++ src/netlify-schematics/index.ts
@@ -2,14 +2,37 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 export function netlifySchematics(options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    const netlifyConfigData = `
-      [build]\n  publish = "${options.publish}"\n
-      command = "${options.command}"\n[[redirects]]\n 
-      from="/*"\n  to="/index.html"\n  status=200
-    `;
+    const chalk = require('chalk');
+    const netlifyConfigData = `[build]\n  publish = "${options.publish}"\n command = "${options.command}"\n[[redirects]]\n from="/*"\n  to="/index.html"\n  status=200`;
 
-    tree.create('/netlify.toml', netlifyConfigData);
+    if (!tree.exists('/netlify.toml')) {
+      tree.create('/netlify.toml', netlifyConfigData);
+      console.log(
+        chalk.green(
+          'A new netlify.toml config file has been created ＿〆(´∀｀●)'
+        )
+      );
+      return tree;
+    }
 
-    return tree;
+    if (options.tomlExists === 'amend') {
+      let tomlBuffer = tree.read('netlify.toml');
+      if (tomlBuffer !== null) {
+        let newToml = `${tomlBuffer.toString()}\n${netlifyConfigData}`;
+        tree.overwrite('netlify.toml', newToml);
+        console.log(
+          chalk.green('Your netlify.toml has been amended ＿〆(´∀｀●)')
+        );
+      }
+      return tree;
+    }
+
+    if (options.tomlExists === 'overwrite') {
+      tree.overwrite('netlify.toml', netlifyConfigData);
+      console.log(
+        chalk.green('Your netlify.toml has been overwritten ＿〆(´∀｀●)')
+      );
+      return tree;
+    }
   };
 }
