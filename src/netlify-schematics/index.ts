0b8e071e68e904665ce99ab1038bd5d848267684
tzmanics <tzmanics@gmail.com>
2020-12-23 15:26:40 -0500

    checks if netlify.toml exists and does not overwrite

--- src/netlify-schematics/index.ts
+++ src/netlify-schematics/index.ts
@@ -2,14 +2,17 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 export function netlifySchematics(options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    const netlifyConfigData = `
-      [build]\n  publish = "${options.publish}"\n
-      command = "${options.command}"\n[[redirects]]\n 
-      from="/*"\n  to="/index.html"\n  status=200
-    `;
+    const chalk = require('chalk');
+    const netlifyConfigData = `[build]\n  publish = "${options.publish}"\n command = "${options.command}"\n[[redirects]]\n from="/*"\n  to="/index.html"\n  status=200`;
 
-    tree.create('/netlify.toml', netlifyConfigData);
+    if (!tree.exists('/netlify.toml')) {
+      tree.create('/netlify.toml', netlifyConfigData);
+      console.log(
+        chalk.green('The netlify.toml config file has been created ＿〆(´∀｀●)')
+      );
+      return tree;
+    }
 
-    return tree;
+    console.log(chalk.bgRed('（ﾉ´д｀）A netlify.toml already exists.'));
   };
 }
