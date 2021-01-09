b7a3b1d04cc37b417c18b28505681817690b4196
tzmanics <tzmanics@gmail.com>
2021-01-08 23:55:07 -0500

    adds execa for ntl calls

--- src/netlify-schematics/index.ts
+++ src/netlify-schematics/index.ts
@@ -2,8 +2,17 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 export function netlifySchematics(options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    const chalk = require('chalk');
     const netlifyConfigData = `[build]\n  publish = "${options.publish}"\n command = "${options.command}"\n[[redirects]]\n from="/*"\n  to="/index.html"\n  status=200`;
+    const chalk = require('chalk');
+    const execa = require('execa');
+
+    console.log(
+      chalk.yellow(
+        '\n(。･∀･)ﾉ゛If you do not currently have a Netlify account, please head to https://app.netlify.com/signup before running the netlify-schematics (´∀｀)♡.\n'
+      )
+    );
+
+    execa('netlify', ['login']).stdout.pipe(process.stdout);
 
     if (!tree.exists('/netlify.toml')) {
       tree.create('/netlify.toml', netlifyConfigData);
